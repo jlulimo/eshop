bc1dc7c58d7b8600c515b8fe2385f17ce3e623e6
jlulimo <jlulimo@163.com>
2017-02-05 16:58:28 +0800

    add web

---
+++ eshop-web/login/login.js
@@ -0,0 +1,36 @@
+$(function() {
+    var pager = {
+        initEvent: function() {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' // optional
+            });
+
+            $("#loginBtn").on('click', function(event) {
+            	var username = $('#username').val();
+            	var password = $('#password').val();
+				$.ajax({
+				        	url: constants.BASE_URL + "login",
+				        	type: 'POST',
+				        	dataType: 'json',
+				        	data: {
+				        		username: username,
+				        		password:password
+				        	},
+				        })
+				        .done(function() {
+				        	console.log("success");
+				        })
+				        .fail(function() {
+				        	console.log("error");
+				        })
+				        .always(function() {
+				        	console.log("complete");
+				        });
+
+            });
+        }
+    }
+    pager.initEvent();
+});

4ca15072544a47db7dff2f6adbecf724b937822f
jlulimo <jlulimo@163.com>
2017-02-09 16:46:05 +0800

    11

--- api-account/src/main/java/com/nnfs/api/account/controller/LoginController.java
+++ api-account/src/main/java/com/nnfs/api/account/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.nnfs.api.account.controller;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,22 +15,23 @@ import com.nnfs.api.account.dto.Result;
 
 @Controller
 public class LoginController {
-//	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
-	@RequestMapping(value="login",method = RequestMethod.POST)
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+	@RequestMapping(value = "login", method = RequestMethod.POST)
 	@ResponseBody
-	public Result Login(@RequestParam(value="username") String username,
-			@RequestParam(value="password") String password,
-			@RequestParam(value="rememberMe") Boolean rememberMe){
+	public Result Login(@RequestParam(value = "username") String username,
+			@RequestParam(value = "password") String password, @RequestParam(value = "rememberMe") Boolean rememberMe) {
 		UsernamePasswordToken token = new UsernamePasswordToken(username, password, rememberMe);
 		try {
 			SecurityUtils.getSubject().login(token);
 		} catch (AuthenticationException e) {
-//			logger.error("Error authenticating.%s",e.getStackTrace());
+			logger.error(String.format("Error authenticating.%s,", e.getStackTrace()));
 			return null;
 		}
 		Result result = new Result();
 		result.setCode(0);
 		result.setMsg("login success.");
+		
 		return result;
 	}
 }

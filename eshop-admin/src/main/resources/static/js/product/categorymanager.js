f83ce8edb8925cd1e124eb7a6b8502c613882460
jlulimo <jlulimo@163.com>
2017-02-24 19:53:20 +0800

    add layout

--- eshop-admin/src/main/resources/static/js/product/categorymanager.js
+++ eshop-admin/src/main/resources/static/js/product/categorymanager.js
@@ -1,16 +1,34 @@
-$(function(){
-	$('#search').keyup(function(){	
-		var current_query = $('#search').val();
-		if (current_query !== "") {
-			$(".list-group li").hide();
-			$(".list-group li").each(function(){
-				var current_keyword = $(this).text();
-				if (current_keyword.indexOf(current_query) >=0) {
-					$(this).show();    	 	
-				};
-			});    	
-		} else {
-			$(".list-group li").show();
-		};
-	});
+$(function() {
+    var categoryManager = {
+    	initUI:function(){
+    		$('#level2BtnGroup,#level3BtnGroup,#level4BtnGroup').hide();
+    		$('#level2,#level3,#level4').selectpicker('hide');
+    	},
+        initEvent: function() {
+            $('#search').keyup(function() {
+                var current_query = $('#search').val();
+                if (current_query !== "") {
+                    $(".list-group li").hide();
+                    $(".list-group li").each(function() {
+                        var current_keyword = $(this).text();
+                        if (current_keyword.indexOf(current_query) >= 0) {
+                            $(this).show();
+                        };
+                    });
+                } else {
+                    $(".list-group li").show();
+                };
+            });
+            $('#level1').change(function() {
+                alert(123);
+            });
+        },
+        init:function(){
+        	categoryManager.initUI();
+        	categoryManager.initEvent();
+        }
+    };
+
+    categoryManager.init();
 });
+

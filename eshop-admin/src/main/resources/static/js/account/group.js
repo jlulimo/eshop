63689f821c257eaa4f149201ca81cfa1e159cd79
jlulimo <jlulimo@163.com>
2017-04-09 15:36:09 +0800

    pemission

--- eshop-admin/src/main/resources/static/js/account/group.js
+++ eshop-admin/src/main/resources/static/js/account/group.js
@@ -1,54 +1,75 @@
 $(function() {
     var page = {
+        initTree: function(treeId) {
+            $('#' + treeId + 'tree').jstree({
+                'core': {
+                    "animation": 0,
+                    "check_callback": true,
+                    'force_text': true,
+                    "themes": { "stripes": true },
+                    "cache": false,
+                    'data': function(obj, callback) {
+                        $.ajax({
+                                url: constants.BASE_URL + '/permission/browse?',
+                                type: 'GET',
+                                dataType: 'json',
+                                data: { nodeId: obj.id },
+                                cache: false,
+                            })
+                            .done(function(result) {
+                                console.log("success");
+                                if (result.code == 0) {
+                                    if (result.data.id == 'root') {
+                                        result.data.id = treeId;
+                                    }
+                                    callback.call(this, result.data);
+                                }
+
+                            })
+                            .fail(function() {
+                                console.log("error");
+                            })
+                            .always(function() {
+                                console.log("complete");
+                            });
+
+                    }
+                },
+                "types": {
+                    "0": { "icon": "glyphicon glyphicon-cloud" },
+                    "root": { "icon": "glyphicon glyphicon-cloud" },
+                    "category": { "icon": "glyphicon glyphicon-file" }
+                },
+                "plugins": ["checkbox", "wholerow"]
+
+            });
+        },
         initEvent: function() {
             $('.nav.nav-tabs li').click(function(event) {
-                /* Act on the event */
-                alert('click: ' + event);
-                var menuId = event.target.id;
-                $('#permissionTree').jstree({
-                    'core': {
-                        "animation": 0,
-                        "check_callback": true,
-                        'force_text': true,
-                        "themes": { "stripes": true },
-                        'data': function(obj, callback) {
-                            $.ajax({
-                                    url: constants.BASE_URL + '/permission/browse?',
-                                    type: 'GET',
-                                    dataType: 'json',
-                                    data: { nodeId: obj.id },
-                                })
-                                .done(function(result) {
-                                    console.log("success");
-                                    if (result.code == 0) {
-                                        if (result.data.id == 'root') {
-                                            result.data.id = menuId;
-                                        }
-                                        callback.call(this, result.data);
-                                    }
+                var href = event.target.href;
+                var temp = href.split('#');
+                var menuId = temp[temp.length - 1];
+                if (menuId != "") {
+                    page.initTree(menuId);
+                }
 
-                                })
-                                .fail(function() {
-                                    console.log("error");
-                                })
-                                .always(function() {
-                                    console.log("complete");
-                                });
-
-                        }
-                    },
-                    "types": {
-                        "0": { "icon": "glyphicon glyphicon-cloud" },
-                        "root": { "icon": "glyphicon glyphicon-cloud" },
-                        "category": { "icon": "glyphicon glyphicon-file" }
-                    },
-                    "plugins": ["checkbox", "wholerow"]
-
-                });
             });
+        },
+
+        init: function() {
+            page.initEvent();
+            var lis = $('.nav.nav-tabs li');
+            for (var i = 0; i < lis.length; i++) {
+                var href = lis[i].children[0].href;
+                var temp = href.split('#');
+                var menuId = temp[temp.length - 1];
+                if (menuId != "") {
+                    page.initTree(menuId);
+                }
+                // alert(menuId);
+            }
         }
     };
 
-    page.initEvent();
-    $('.nav.nav-tabs li')[0].click();
+    page.init();
 })

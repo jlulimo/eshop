9de143cdb2003c6462c6bed7d828dd54c247b085
jlulimo <jlulimo@163.com>
2017-04-14 17:03:35 +0800

    permissions

--- api-account/src/main/java/com/nnfs/api/account/service/impl/GroupServiceImpl.java
+++ api-account/src/main/java/com/nnfs/api/account/service/impl/GroupServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nnfs.api.account.dao.GroupDao;
-import com.nnfs.api.account.dao.GroupPermissionDao;
 import com.nnfs.api.account.domain.Group;
 import com.nnfs.api.account.dto.GroupDto;
 import com.nnfs.api.account.service.GroupService;
